99625ad267b56318be35ead089ff34f434617a4e
HH <a2013hh@aiit.ac.jp>
2020-07-03 23:51:57 +0900

    modified all file

--- gcd-test.sh
+++ gcd-test.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-result=$(./gcd.sh)
-echo $result
-if [ $result = 2 ] ; then
-	exit 0
-else 
-	exit 1
-fi
+#実行結果(2と４を入力した場合)
+./gcd.sh 2 4 > /tmp/result-$$
+#出てほしい出力
+echo 2 > /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ || exit 1
+
+#実行結果(3を入力した場合)
+./gcd.sh 3 > /tmp/result-$$
+#出てほしい出力
+echo "error" > /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ || exit 1
+
+#実行結果(文字を入力した場合)
+./gcd.sh a b> /tmp/result-$$
+echo "error" > /tmp/ans-$$
+
+diff /tmp/ans-$$ /tmp/result-$$ || exit 1
+
+echo "complete"
+
+
+#result=$(./gcd.sh)
+#echo $result
+#if [ $result = 2 ] ; then
+#	exit 0
+#else 
+#	exit 1
+#fi
